e5d479743b681742707dae51586c9600a85087ad
Rohit <rohitgha@cybage.com>
2023-03-15 17:09:07 +0530

    ecommerce app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+// import * as Icons from "react-icons/gi";
+
+
+function App() {
+  return (
+    <Fragment>
+      <h1>---Ecommerce Website---</h1>
+    </Fragment>
+  );
+}
+
+export default App;
